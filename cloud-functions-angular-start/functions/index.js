bc43fa3f9ca1e6095fea35ff2ba02c46accedd7c
fusho-takahashi <zenryaku@fusho-takahashi.me>
2020-01-09 16:41:29 +0900

    初回ログインメッセージを表示する functions を追加

--- cloud-functions-angular-start/functions/index.js
+++ cloud-functions-angular-start/functions/index.js
@@ -15,9 +15,21 @@
  */
 
 // TODO(DEVELOPER): Import the Cloud Functions for Firebase and the Firebase Admin modules here.
-
+const functions = require('firebase-functions');
+const admin = require('firebase-admin')
 // TODO(DEVELOPER): Write the addWelcomeMessages Function here.
+exports.addWelcomeMessages = functions.auth.user().onCreate((user) => {
+  console.log('A new user signed in for the first time');
 
+  const fullName = user.displayName || 'Anonymous';
+
+  return admin.database().ref('message').push({
+    name: 'Firebase Bot',
+    photoUrl: '/assets/images/firebase-logo.png',
+    text: `${fullName} signed in for the first time! Welcome!`
+  })
+
+})
 // TODO(DEVELOPER): Write the blurOffensiveImages Function here.
 
 // TODO(DEVELOPER): Write the sendNotifications Function here.
